54234f32877c91c3ef37b9d2b063043dbca6f7d4
vladimir-rurik <36802394+vladimir-rurik@users.noreply.github.com>
2018-09-11 17:53:49 +0300

    oop style fix in application module.

--- js/application.js
+++ js/application.js
@@ -3,30 +3,30 @@ import GameModel from './data/game-model';
 import GameScreen from './screens/game-screen';
 import ResultScreen from './screens/result-screen';
 
-/**
- * Рендер экран приложения
- * @param {Node} element - Элемент экрана
- */
-const showScreen = (element) => {
-  const mainScreen = document.querySelector(`.main`);
-  mainScreen.parentNode.replaceChild(element, mainScreen);
-};
-
 /** Класс для управления экранами игры */
 export default class Application {
+  /**
+   * Рендер экран приложения
+   * @param {Node} element - Элемент экрана
+   */
+  static _showScreen(element) {
+    const mainScreen = document.querySelector(`.main`);
+    mainScreen.parentNode.replaceChild(element, mainScreen);
+  }
+
   static showWelcome() {
     const welcome = new WelcomeScreen();
-    showScreen(welcome.element);
+    this._showScreen(welcome.element);
   }
 
   static showGame() {
     const gameScreen = new GameScreen(new GameModel());
-    showScreen(gameScreen.element);
+    this._showScreen(gameScreen.element);
     gameScreen.startGame();
   }
 
   static showStats(screenType, stats) {
     const resultScreen = new ResultScreen(screenType, stats);
-    showScreen(resultScreen.element);
+    this._showScreen(resultScreen.element);
   }
 }
